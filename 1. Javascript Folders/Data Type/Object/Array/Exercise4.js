ec95c42c4790f6a087ed14885174743cbd96dc66
Ariel Izurieta <ariel_izurieta@hotmail.com>
2023-06-24 21:33:21 -0300

    4th exercise data type array js

---
+++ 1. Javascript Folders/Data Type/Object/Array/Exercise4.js
@@ -0,0 +1,13 @@
+var array_1 = [0,1,2,3,4,5]
+
+for (number in array_1)
+{
+    if (number % 2 == 0)
+    {
+        console.log("The number ", number, "is par")
+    }
+    else
+    {
+        console.log("The number ", number, "is impar")
+    }
+}
