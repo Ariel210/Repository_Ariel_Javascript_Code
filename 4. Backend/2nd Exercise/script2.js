e74bc07a16c79f35b9a09c7b3ed07a096bc68b4e
Ariel Izurieta <ariel_izurieta@hotmail.com>
2023-06-30 19:30:46 -0300

    2nd exercise backend js & html calculator

---
+++ 4. Backend/2nd Exercise/script2.js
@@ -0,0 +1,8 @@
+let number_1 = parseInt(prompt('Enter 1st number: '));
+let number_2 = parseInt(prompt('Enter 2nd number: '));
+
+
+console.log("--Calculator--")
+console.log("Sum Result: ", number_1 + number_2)
+console.log("Product Result: ", number_1 * number_2)
+console.log("Division Result: ", number_1 / number_2)
