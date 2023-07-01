924cd9e4a181de72e055540765adc2c962fb6e2c
Ariel Izurieta <ariel_izurieta@hotmail.com>
2023-07-01 18:40:48 -0300

    3rd exercise backend Html & js

---
+++ 4. Backend/3rd Exercise/script3.js
@@ -0,0 +1,11 @@
+function third_exercise()
+{
+    nombre = prompt("Enter your name: ")
+    surname = prompt("Enter your surname: ")
+
+    console.log(nombre);
+    console.log(surname)
+    console.log("Complete Dates: " + nombre + ' ' + surname)
+}
+
+third_exercise()
