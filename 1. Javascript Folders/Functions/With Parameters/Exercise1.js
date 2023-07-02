f8403c36c9693b7bb6f717d8856a6548d60643d3
Ariel Izurieta <ariel_izurieta@hotmail.com>
2023-07-02 13:31:53 -0300

    1st exercise function with parameters js

---
+++ 1. Javascript Folders/Functions/With Parameters/Exercise1.js
@@ -0,0 +1,10 @@
+// 1st Exercise | Sum of the two numbers in JavaScript
+
+let number_1 = 10;
+let number_2 = 10;
+
+function first_exercise (number_1, number_2)
+{
+    console.log("Sum: ", number_1 + number_2)
+}
+first_exercise(number_1, number_2)
